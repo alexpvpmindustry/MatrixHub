afd43404d678426d2cfc97e49f035d175ce38efe
alex.mindustry <alex.mindustry@gmail.com>
2020-12-23 17:45:01 +0800

    working version

--- src/main/java/matrixHub/Main.java
+++ src/main/java/matrixHub/Main.java
@@ -1,7 +1,6 @@
 package matrixHub;
 
 import arc.Events;
-import arc.net.Server;
 import arc.util.*;
 
 import matrixHub.utils.*;
@@ -9,59 +8,83 @@ import matrixHub.utils.*;
 import mindustry.Vars;
 import mindustry.game.EventType.*;
 import mindustry.gen.Call;
+import mindustry.gen.Groups;
 import mindustry.mod.Plugin;
+import mindustry.gen.Player;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main extends Plugin{
 
     public Main() {
-
+        final String servertitle="WELCOME TO [red]A[yellow]L[teal]E[blue]X [gold]HUB";
         Config.main();
-
+        //AtomicInteger time = new AtomicInteger();
         Events.on(ServerLoadEvent.class, event -> {
             Vars.netServer.admins.addActionFilter(playerAction -> false);
-            //Call.onLabel(ConfigTranslate.get("server1.title"), 1100f, 487f, 306f);
+
+            Timer.schedule(()->{
+                for (int i = 0; i < Groups.player.size(); i++) {
+                    Player p = Groups.player.index(i);
+                    checkAndConnect(p);
+                }
+            },0.5f,0.5f);
         });
 
         Events.on(PlayerJoin.class, event -> {
-
-            Upd upd = new Upd();
-            upd.main(event.player);
-
-            Call.label(event.player.con, ConfigTranslate.get("server4.title"), 1100f, 508f, 304f);
-            //Call.onLabel(event.player.con, ConfigTranslate.get("server3.title"), 1100f, 284f, 304f);
-            //Call.onLabel(event.player.con, ConfigTranslate.get("server2.title"), 1100f, 508f, 529f);
-            //Call.onLabel(event.player.con, ConfigTranslate.get("server1.title"), 1100f, 284f, 529f);
-
-//            Vars.net.pingHost(Config.get("ip1"), Integer.parseInt(Config.get("port1")), host -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.online").replace("{0}", String.valueOf(host.players)), 1100f, 284f, 490f);
-//            }, e -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.offline"), 1100f, 284f, 490f);
-//            });
-//
-//            Vars.net.pingHost(Config.get("ip2"), Integer.parseInt(Config.get("port2")), host -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.online").replace("{0}", String.valueOf(host.players)), 1100f, 508f, 490f);
-//            }, e -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.offline"), 1100f, 508f, 490f);
-//            });
-//
-//            Vars.net.pingHost(Config.get("ip3"), Integer.parseInt(Config.get("port3")), host -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.online").replace("{0}", String.valueOf(host.players)), 1100f, 284f, 265f);
-//            }, e -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.offline"), 1100f, 280f, 265f);
-//            });
-//
-//            Vars.net.pingHost(Config.get("ip4"), Integer.parseInt(Config.get("port4")), host -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.online").replace("{0}", String.valueOf(host.players)), 1100f, 508f, 265f);
-//            }, e -> {
-//                Call.onLabel(event.player.con, ConfigTranslate.get("server.offline"), 1100f, 508f, 265f);
-//            });
-
+            //Call.label(event.player.con, ConfigTranslate.get("server4.title"), 1100f, 508f, 304f);
+            Call.label( servertitle, 1100f, 200f, 220f);
+            Call.label(event.player.con, "PVP", 1100f, 350f, 350f);
+            Call.label(event.player.con, "SANDBOX", 1100f, 50f, 50f);
+            Call.label(event.player.con, "(but its actually PVP)", 1100f, 50f, 40f);
+            Call.label(event.player.con, "ATTACK", 1100f, 50f, 350f);
+            Call.label(event.player.con, "(but its actually PVP)", 1100f, 50f, 340f);
+            Call.label(event.player.con, "SURVIVAL", 1100f, 350f, 50f);
         });
+
+    }
+
+    public void checkAndConnect(Player player){
+        if (player.x <= 100f && player.x >= 10f && player.y >= 10f && player.y <= 100f) { // dont do exactly (0,0)
+
+            Vars.net.pingHost("alexmindustry.ddns.net", 6568, host -> {
+                Call.connect(player.con, "alexmindustry.ddns.net", 6568);
+            }, e -> {
+            });
+        }
+        //if(true)//Boolean.parseBoolean(Config.get("server2Works")))
+        if (player.x <= 100f && player.x >= 0f && player.y >= 300f && player.y <= 400f) {
+            Vars.net.pingHost("alexmindustry.ddns.net", 6568, host -> {
+                Call.connect(player.con, "alexmindustry.ddns.net", 6568);
+            }, e -> {
+            });
+        }
+        if (player.x <= 400f && player.x >= 300f && player.y >= 300f && player.y <= 400f) { //upper right
+            Call.connect(player.con, "alexmindustry.ddns.net", 6568);
+        }
+        if (player.x <= 400f && player.x >= 300f && player.y >= 0f && player.y <= 100f) {
+            Call.connect(player.con, "alexmindustry.ddns.net", 6569);
+        }
     }
 
     @Override
     public void registerServerCommands(CommandHandler handler){
         handler.register("ping", "Return \"Pong!\"", args -> { Log.info("Pong!");});
+    }
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("reply25", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+        });
+        //register a simple reply command
+        handler.<Player>register("survival", "<text...>", "command to jump to survival.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+            Call.connect(player.con,"alexmindustry.ddns.net",6569);
+        });
+        handler.<Player>register("pvp", "<text...>", "command to jump to survival.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+            Call.connect(player.con,"http://alexmindustry.ddns.net",6568);
+        });
     }
 
 }
